8744a96cd87ed1bf673fcce4d8b87d9b555bf860
Eldaradelshin <e.adelshin@ya.ru>
2018-11-22 21:54:14 +0300

    starting project

---
+++ mobileTMDB/mobileTMDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mobileTMDB
+//
+//  Created by rushan adelshin on 22.11.2018.
+//  Copyright © 2018 Eldar Adelshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
